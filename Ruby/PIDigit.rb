2e05a7f9d5b44cf99ef877ace3ceccd1ffe82191
Andrew Jarombek <ajarombek95@gmail.com>
2016-07-19 21:09:55 -0400

    Ruby Program #1 - PIDigit (In-Progress)

---
+++ Ruby/PIDigit.rb
@@ -0,0 +1,19 @@
+# Author: Andrew Jarombek
+# Date: 7/19/2016
+# Enter a number and have the program generate PI up to that many decimal places.
+# Keep a limit to how far the program will go.
+
+class PIDigit
+    # Ruby uses snakecase for varaible/method names 
+    def get_pi_digit(digits)
+        # Get the PI constant from the math library
+        pi = Math::PI
+        # You can put variables into strings by using the #{} command
+        puts "PI with #{digits} digits is %.#{digits}f" % pi
+    end
+end
+
+# Get the first command line argument
+dig = ARGV[0]
+p = PIDigit.new
+p.get_pi_digit(dig)
