2b678890f13d6d09adb5f28e802e4df7a960ce16
Andrew Jarombek <ajarombek95@gmail.com>
2019-05-29 18:39:36 -0400

    refactor repo, add C# events

---
+++ C#/base/events/Events/Program.cs
@@ -0,0 +1,43 @@
+﻿/**
+ * Investigate Events in C#
+ * Sources: [https://stackoverflow.com/a/803320]
+ * Author: Andrew Jarombek
+ * Date: 5/29/2019
+ */
+
+using System;
+
+namespace Events
+{
+    class Program
+    {
+        // Delegate to used as the signature for the event
+        public delegate void FloatHandler(double num);
+
+        static void Main(string[] args)
+        {
+            Broadcaster.Run(5.21); // I Ran 5.21 Miles
+        }
+    }
+
+    class Broadcaster
+    {
+        // Event which holds a single delegate instance - RunOccurred
+        public static event Program.FloatHandler RunHandler = RunOccurred;
+
+        /// <summary>
+        /// Function which prints out the number of miles run.  Conforms to the FloatHandler delegate signature.
+        /// </summary>
+        /// <param name="num">The distance run.</param>
+        private static void RunOccurred(double num) => Console.Write($"I Ran {num} Miles");
+
+        /// <summary>
+        /// Function which triggers the event with a certain run distance.
+        /// </summary>
+        /// <param name="num">The distance run.</param>
+        public static void Run(double num)
+        {
+            RunHandler(num);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# mini-practice-problems
+# misc-code-samples
+
+> *Formerly mini-practice-problems*
+
+### Overview
+
+This repository contains pieces of code which didn't make it into the [jarombek-com-sources](https://github.com/AJarombek/jarombek-com-sources)
+repository because they don't contain enough material to write a blog post.  I still want to keep them in source control because they are
+important reference items.
