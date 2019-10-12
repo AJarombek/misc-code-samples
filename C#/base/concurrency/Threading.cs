186112ecdae22560a44b07aaf3fe1c88ac173147
AJarombek <ajarombek95@gmail.com>
2019-10-12 13:28:56 -0400

    basic locks for multi-threading

--- C#/base/concurrency/Threading.cs
+++ C#/base/concurrency/Threading.cs
@@ -13,6 +13,9 @@ namespace concurrency
 {
     public static class Threading
     {
+        private static int _count;
+        private static readonly object _locker = new object();
+        
         public static void ExecuteBasicThread()
         {
             Thread thread = new Thread(() => Print('1'));
@@ -22,6 +25,11 @@ namespace concurrency
             Print('0');
         }
 
+        /// <summary>
+        /// Function called by a thread which prints out a character 50 times.  Each thread gets its own memory stack,
+        /// so each gets its own variable 'i'.
+        /// </summary>
+        /// <param name="c">The character to print out.</param>
         static void Print(char c)
         {
             for (int i = 0; i < 50; i++)
@@ -40,6 +48,44 @@ namespace concurrency
             thread.Join();
             
             Assert(!thread.IsAlive);
+            
+            // Multi-threading is dangerous when two or more threads share mutable state.  In this case,
+            // the mutable state is 'count'.
+            Thread dangerousThread = new Thread(() => {
+                for (int i = 0; i < 10000; i++) { _count++; }
+            });
+            dangerousThread.Start();
+            
+            for (int i = 0; i < 10000; i++) { _count++; }
+
+            dangerousThread.Join();
+            
+            // 'count' will not always print out 20000.  To avoid this issue, use locking.  Or if possible don't
+            // introduce mutable shared state.
+            Console.WriteLine($"\nUnsafe Count: {_count}"); 
+            
+            // This example performs the same operation in a thread-safe manner with a locking object.
+            _count = 0;
+            Thread safeThread = new Thread(Add);
+            safeThread.Start();
+            Add();
+
+            safeThread.Join();
+            Console.WriteLine($"Safe Count: {_count}"); 
+        }
+
+        /// <summary>
+        /// Increment a shared integer in a thread-safe manner using locks.
+        /// </summary>
+        static void Add()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                lock (_locker)
+                {
+                    _count++;
+                }
+            }
         }
     }
 }
--- Java/base/concurrency/Threading.java
+++ Java/base/concurrency/Threading.java
@@ -1,5 +1,8 @@
 package concurrency;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Comparing Java's threading functionality to languages I'm learning.
  * @author Andrew Jarombek
@@ -7,6 +10,10 @@ package concurrency;
  */
 
 class Threading {
+
+    private static int count;
+    private static final Lock LOCK = new ReentrantLock();
+
     static void executeBasicThread() {
         var thread = new Thread(() -> print('1'));
         assert !thread.isAlive();
@@ -15,6 +22,10 @@ class Threading {
         print('0');
     }
 
+    /**
+     * Function called by a thread which prints out a character 50 times.
+     * @param c The character to print out.
+     */
     private static void print(char c) {
         for (int i = 0; i < 50; i++)
         {
@@ -39,6 +50,48 @@ class Threading {
             assert !thread.isAlive();
         } catch (InterruptedException e) {
             assert false;
+        }
+
+        // This thread isn't safe because it mutates the 'count' variable which is shared with other threads.
+        Thread dangerousThread = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) { count++; }
+        });
+
+        dangerousThread.start();
+        for (int i = 0; i < 10000; i++) { count++; }
+
+        try {
+            dangerousThread.join();
+
+            // This won't always print 20000.
+            System.out.println("\nUnsafe Count: " + count);
+        } catch (InterruptedException e) {
+            assert false;
+        }
+
+        count = 0;
+
+        // This example performs the same operation in a thread-safe manner with a re-entrant locking object
+        Thread safeThread = new Thread(Threading::add);
+        safeThread.start();
+        add();
+
+        try {
+            safeThread.join();
+            System.out.println("Safe Count: " + count);
+        } catch (InterruptedException e) {
+            assert false;
+        }
+    }
+
+    /**
+     * Increment a shared integer in a thread-safe manner using a re-entrant lock.
+     */
+    private static void add() {
+        for (int i = 0; i < 10000; i++) {
+            LOCK.lock();
+            count++;
+            LOCK.unlock();
         }
     }
 }
