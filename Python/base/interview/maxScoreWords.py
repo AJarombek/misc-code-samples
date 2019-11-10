46c24c61b1bfca5dfe08c70c8b9388158c3a1e9e
Andrew Jarombek <ajarombek95@gmail.com>
2019-11-10 15:43:52 -0500

    difficult max score words problem kinda done

--- Python/base/interview/maxScoreWords.py
+++ Python/base/interview/maxScoreWords.py
@@ -1,8 +1,27 @@
+"""
+Interview practice problem: https://leetcode.com/problems/merge-two-binary-trees/
+Result: [FAILURE - 2:00]
+Description:
+    Given a list of words, list of  single letters (might be repeating) and score of every character.  Return the
+    maximum score of any valid set of words formed by using the given letters (words[i] cannot be used two or more
+    times).
+
+Author: Andrew Jarombek
+Date: 11/10/2019
+"""
+
 import collections
 import itertools
 
 
-def max_score_words(self, words: list, letters: list, score: list) -> int:
+def max_score_words_original(words: list, letters: list, score: list) -> int:
+    """
+    Try to solve the problem using brute force.  This method was too slow to be accepted.
+    :param words: A list of words.
+    :param letters: A list of lowercase letters.
+    :param score: A list of scores for each lowercase letter.
+    :return: The maximum score for a combination of words.
+    """
     max_points = 0
 
     letters_dict = collections.defaultdict(int)
@@ -28,10 +47,20 @@ def max_score_words(self, words: list, letters: list, score: list) -> int:
     return max_points
 
 
-class Solution:
+class MaxScoreWordsWithMemoization:
+    # Use memoization as an optimization technique to save prior results
     memo = {}
 
-    def maxScoreWords(self, words: list, letters: list, score: list) -> int:
+    def max_score_words(self, words: list, letters: list, score: list) -> int:
+        """
+        Try to solve the problem using recursion and memoization.  The approach is still brute-force, however the
+        hope was that caching results with memoization would speed things up enough to be accepted.  It still wasn't
+        fast enough.
+        :param words: A list of words.
+        :param letters: A list of lowercase letters.
+        :param score: A list of scores for each lowercase letter.
+        :return: The maximum score for a combination of words.
+        """
         max_points = 0
 
         letters_dict = collections.defaultdict(int)
@@ -42,29 +71,34 @@ class Solution:
             perms = itertools.permutations(words, r=i)
 
             for perm in perms:
-                _, _, points = self.maxScoreWordsSubset(perm, letters_dict.copy(), score, 0)
-                print(''.join(perm))
-                print(points)
+                _, _, points = self.max_score_words_subset(perm, letters_dict.copy(), score, 0)
                 max_points = max(max_points, points)
 
         return max_points
 
-    def maxScoreWordsSubset(self, words, letters_dict, score_list, score):
+    def max_score_words_subset(self, words: list, letters_dict: dict, score_list: list, score: int) -> tuple:
+        """
+        Helper function which is called recursively.
+        :param words: A list of words.
+        :param letters_dict: A dictionary mapping a letter to the number of times it can be used.
+        :param score_list: A list of scores for each lowercase letter.
+        :param score: The current score of the words combination.
+        :return: A tuple containing the updated words, letters_dict, and score parameters.
+        """
         if len(words) == 0:
             return words, letters_dict, score
         if score == -1:
             return [], {}, -1
 
         words_str = ''.join(words)
-        letters_str = ''.join(letters_dict)
-        if (words_str, letters_str) in self.memo:
-            return self.memo[(words_str, letters_str)]
+        if (words_str, score) in self.memo:
+            return self.memo[words_str, score]
         else:
             word = words[0]
 
             if len(words) > 1:
-                new_words, new_letters_dict, new_score = self.maxScoreWordsSubset(words[1:], letters_dict, score_list,
-                                                                                  score)
+                new_words, new_letters_dict, new_score = \
+                    self.max_score_words_subset(words[1:], letters_dict, score_list, score)
                 words = new_words
                 letters_dict = new_letters_dict
                 score = new_score
@@ -79,10 +113,34 @@ class Solution:
                 else:
                     return [], {}, -1
 
-            self.memo[(''.join(words), ''.join(letters_dict))] = (words, letters_dict, score)
+            self.memo[''.join(words), score] = (words, letters_dict, score)
             return words, letters_dict, score
 
 
 if __name__ == '__main__':
-    solution = Solution()
-    result = solution.maxScoreWords(["xxxz","ax","bx","cx"], ["z","a","b","c","x","x","x"], [4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,0,10])
+    # Test my initial solution
+    max_points = max_score_words_original(
+        ["xxxz", "ax", "bx", "cx"],
+        ["z", "a", "b", "c", "x", "x", "x"],
+        [4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 10]
+    )
+    assert max_points == 27
+
+    solution = MaxScoreWordsWithMemoization()
+
+    # Test the helper function for the solution with Memoization
+    _, _, points = solution.max_score_words_subset(
+        ['ax', 'bx'],
+        {'a': 1, 'b': 1, 'c': 1, 'x': 3, 'z': 1},
+        [4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 10],
+        0
+    )
+    assert points == 18
+
+    # Test the solution with Memoization
+    max_points = solution.max_score_words(
+        ["xxxz", "ax", "bx", "cx"],
+        ["z", "a", "b", "c", "x", "x", "x"],
+        [4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 10]
+    )
+    assert max_points == 27
