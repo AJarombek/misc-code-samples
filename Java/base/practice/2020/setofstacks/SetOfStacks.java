735e08234dcecc75a029d364e96d8635e7f8cea1
Andrew Jarombek <ajarombek95@gmail.com>
2020-01-02 18:21:11 -0500

    java set of stacks problem unit tests

--- Java/base/practice/2020/setofstacks/SetOfStacks.java
+++ Java/base/practice/2020/setofstacks/SetOfStacks.java
@@ -24,12 +24,20 @@ public class SetOfStacks<T> {
     private List<Stack<T>> stacks;
     private int max;
 
+    /**
+     * Construct a new set of stacks.
+     */
     public SetOfStacks() {
         max = 5;
         stacks = new ArrayList<>();
         stacks.add(new Stack<>());
     }
 
+    /**
+     * Push an item onto the stacks.  If the final stack in the set is at max capacity, a new stack is added to
+     * the set.
+     * @param item The item that is pushed onto the stack.
+     */
     public void push(T item) {
         Stack<T> stack = stacks.get(stacks.size() - 1);
 
@@ -42,6 +50,11 @@ public class SetOfStacks<T> {
         }
     }
 
+    /**
+     * Pop an item off of the stack and return it.  If the final stack in the set is empty after the pop operation,
+     * delete it from the set.
+     * @return The item popped off the stacks.
+     */
     public T pop() {
         Stack<T> stack = stacks.get(stacks.size() - 1);
         T result = stack.pop();
@@ -53,6 +66,12 @@ public class SetOfStacks<T> {
         return result;
     }
 
+    /**
+     * Pop an item off of a specific stack in the sets.  If the stack is empty at the end of the operation, it
+     * is removed from the set.
+     * @param index Index of a stack in the set.
+     * @return The item popped off the stacks.
+     */
     public T popAt(int index) {
         Stack<T> stack = stacks.get(index);
         T result = stack.pop();
--- Java/base/practice/2020/setofstacks/SetOfStacksTest.java
+++ Java/base/practice/2020/setofstacks/SetOfStacksTest.java
@@ -1,11 +1,51 @@
 package setofstacks;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
+
+/**
+ * Unit Tests: Cracking the Coding Interview: Question 3.3
+ * @author Andrew Jarombek
+ * @since 1/2/2020
+ */
 
 class SetOfStacksTest {
 
     @Test
-    void test() {
+    void testEmptyStack() {
+        SetOfStacks<Integer> stacks = new SetOfStacks<>();
+        assertThrows(EmptyStackException.class, stacks::pop);
+        assertThrows(EmptyStackException.class, () -> stacks.popAt(0));
+    }
 
+    @Test
+    void testPushOnce() {
+        SetOfStacks<Integer> stacks = new SetOfStacks<>();
+        stacks.push(1);
+        assertEquals(1, stacks.pop());
+    }
+
+    @Test
+    void testMultipleStacks() {
+        SetOfStacks<Integer> stacks = new SetOfStacks<>();
+        stacks.push(1);
+        stacks.push(2);
+        stacks.push(3);
+        stacks.push(4);
+        stacks.push(5);
+        stacks.push(6);
+        stacks.push(7);
+
+        assertEquals(5, stacks.popAt(0));
+        assertEquals(4, stacks.popAt(0));
+        assertEquals(7, stacks.popAt(1));
+
+        assertEquals(6, stacks.pop());
+        assertEquals(3, stacks.pop());
+        assertEquals(2, stacks.pop());
+        assertEquals(1, stacks.pop());
     }
 }
