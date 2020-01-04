cb3c38a7bec43e8db2be33f7e2b811399cdeb5b2
Andrew Jarombek <ajarombek95@gmail.com>
2020-01-04 12:37:31 -0500

    +

--- Python/base/practice/2020/uniqueEmail/testUniqueEmailAddresses.py
+++ Python/base/practice/2020/uniqueEmail/testUniqueEmailAddresses.py
@@ -6,6 +6,11 @@ Date: 1/4/2020
 
 # If that is making you happy I am glad for you.  Of course, I just want happiness for you.  It sounds like someone
 # else is making you happy and making you feel loved and more.  I'll still always be here for you if you ever need me.
+#
+# You will always have all my love and support.  You mean the world to me and I'm so grateful to have spent so much time
+# with you.  Even if we never spend time together again, I will always be rooting for you and praying for your success.
+# All I really know if that you deserve to be loved by whoever you love.  If any of those feelings you had for me still
+# remain, you can rest assured that I still love you with all my heart.
 # -Andy
 
 import unittest
