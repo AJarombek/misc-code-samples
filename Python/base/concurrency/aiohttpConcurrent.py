073950de1cb7f8820579ad1ecef836997b8966de
AJarombek <ajarombek95@gmail.com>
2020-10-04 15:44:32 -0400

    more asyncio + aiohttp

---
+++ Python/base/concurrency/aiohttpConcurrent.py
@@ -0,0 +1,51 @@
+"""
+Demonstrate calling an API with asyncio.
+Author: Andrew Jarombek
+Date: 10/4/2020
+"""
+
+import asyncio
+import time
+from typing import Tuple
+
+import aiohttp
+
+
+async def make_request(session: aiohttp.ClientSession, endpoint: str) -> Tuple[str, int]:
+    """
+    Make an API request to and endpoint on a mocked REST API.
+    :param session: An aiohttp session interface for making HTTP requests.
+    :param endpoint: The endpoint on the API to make a GET request to.
+    :return: The response JSON of the API call and the HTTP status code.
+    """
+    url = f'https://jsonplaceholder.typicode.com/{endpoint}'
+    async with session.get(url) as response:
+        return await response.json(), response.status
+
+
+async def make_requests() -> int:
+    endpoints = ['posts', 'comments', 'albums', 'photos', 'todos', 'users']
+    success_count = 0
+
+    async with aiohttp.ClientSession() as session:
+        for endpoint in endpoints:
+            response: Tuple[str, int] = await make_request(session, endpoint)
+            print(response[1])
+
+            if response[1] == 200:
+                success_count += 1
+
+    return success_count
+
+
+def main() -> None:
+    start = time.time()
+    loop = asyncio.get_event_loop()
+    success_count = loop.run_until_complete(make_requests())
+    end = time.time()
+    print(f'API calls completed in: {end - start}')
+    print(f'Success count: {success_count}')
+
+
+if __name__ == '__main__':
+    main()
--- Python/base/concurrency/asyncioCoroutines.py
+++ Python/base/concurrency/asyncioCoroutines.py
@@ -5,59 +5,97 @@ Date: 10/3/2020
 """
 
 import asyncio
+import time
+from typing import Tuple, List
 
 """
 Asyncio example in Python 3.5+
 """
 
 
-async def predicted_sunday_run_length():
+async def predicted_sunday_run_length() -> float:
+    """
+    Simulate a long running operation that predicts the length of my Sunday long run.
+    """
     await asyncio.sleep(1)
     return 12.31
 
 
-async def predicted_weekly_mileage():
+async def predicted_weekly_mileage() -> float:
+    """
+    Simulate a long running operation that predicts my weekly running mileage.
+    """
     await asyncio.sleep(2)
     return 44
 
 
-async def running_predictions():
+async def running_predictions() -> Tuple[float, float]:
+    """
+    Using async/await, call both the simulated functions.
+    """
     sunday_run = await predicted_sunday_run_length()
     weekly_mileage = await predicted_weekly_mileage()
     return sunday_run, weekly_mileage
 
 
-def main():
+async def running_predictions_concurrent() -> List[float]:
+    """
+    The same as running_predictions() above, except it uses asyncio.gather() instead of two await statements.
+    """
+    return await asyncio.gather(predicted_weekly_mileage(), predicted_sunday_run_length())
+
+
+def main() -> None:
+    """
+    Using asyncio, call the async function running_predictions.
+    """
+    start = time.time()
     sunday_run, weekly_mileage = asyncio.run(running_predictions())
+    end = time.time()
     print(sunday_run)
     print(weekly_mileage)
 
+    # Two awaits completes in 3 seconds.
+    print(f'two awaits completes in: {end - start}')
+
+    start = time.time()
+    predictions = asyncio.run(running_predictions_concurrent())
+    end = time.time()
+    print(predictions[0])
+    print(predictions[1])
+
+    # await asyncio.gather completes in 2 seconds.
+    print(f'await asyncio.gather() completes in: {end - start}')
+
 
 """
-Asyncio example in Python 3.4
+Asyncio example in Python 3.4.
+This is the same as Python 3.5+ except the @asyncio.coroutine annotation is used instead of the async function syntax, 
+and 'yield from' is used instead of the await keyword.
 """
 
 
 @asyncio.coroutine
-def predicted_sunday_run_length_deprecated():
+def predicted_sunday_run_length_deprecated() -> float:
     yield from asyncio.sleep(1)
     return 13.5
 
 
 @asyncio.coroutine
-def predicted_weekly_mileage_deprecated():
+def predicted_weekly_mileage_deprecated() -> float:
     yield from asyncio.sleep(2)
     return 48
 
 
 @asyncio.coroutine
-def running_predictions_deprecated():
+def running_predictions_deprecated() -> Tuple[float, float]:
     sunday_run = yield from predicted_sunday_run_length()
     weekly_mileage = yield from predicted_weekly_mileage()
     return sunday_run, weekly_mileage
 
 
-def main_deprecated():
+def main_deprecated() -> None:
+    start = time.time()
     try:
         loop = asyncio.get_event_loop()
     except:
@@ -65,9 +103,13 @@ def main_deprecated():
 
     sunday_run, weekly_mileage = loop.run_until_complete(running_predictions_deprecated())
     loop.close()
+    end = time.time()
 
     print(sunday_run)
     print(weekly_mileage)
+
+    # Two awaits in Python 3.4 also completes in 3 seconds.
+    print(f'two awaits (python 3.4) completes in: {end - start}')
 
 
 if __name__ == '__main__':
